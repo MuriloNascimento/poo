0c4ce4c14fc3b44f74ecf94f78fa6bd757c85f8f
Murilo Nascimento <murilo.nascimento1988@gmail.com>
2017-12-21 16:15:19 -0200

    Update index.php

--- index.php
+++ index.php
@@ -9,3 +9,29 @@ include './smConfig.php';
 $sm->assign("nome","Murilo");
 
 $sm->display('index.tpl');
+
+
+- carro
+nome
+cor
+data_compra
+quilometragem
+
+
+- categoria
+nome
+
+
+- peca
+categoria
+nome
+
+
+- revisao
+carro
+peca
+data_troca
+custo_peca
+curto_mao_de_obra
+quilometragem
+quilometragem_proxima_troca
